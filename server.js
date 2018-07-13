c0a11bc00d38a0f81910dbbdc85b702d3bdcee2e
Kyle Nyland <knyland@ameritech.net>
2018-07-13 16:03:18 -0400

    In progress: Adding dialog box and text when talking to npc

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ gifEncoder = require('gifencoder');
 var T = new Twit(config);
 
 var mostRecentBotTweet = [];
-var tweets = [];
+var tweets = [{"text":"@NintendoAmerica talk", "favorite_count":999999}];
 
 var gameState = JSON.parse(fs.readFileSync('gameState.json', 'utf8'));
 
 //Get most recent bot tweet to determine search results for tweets since then
-T.get('search/tweets', {q: 'from:RubberNinja', count: 1, result_type: 'recent'}, function(err, data, response){
+T.get('search/tweets', {q: 'from:NintendoAmerica', count: 1, result_type: 'recent'}, function(err, data, response){
 	if (data.statuses.length > 0){
 		//A bot tweet was found, get all mentions since then
 		mostRecentBotTweet = data.statuses[0];
-		T.get('search/tweets', { q: 'to:RubberNinja', count: 100, since_id: mostRecentBotTweet.id }, function(err, data, response){
+		T.get('search/tweets', { q: 'to:NintendoAmerica', count: 100, since_id: mostRecentBotTweet.id }, function(err, data, response){
 			for (var i = 0; i < data.statuses.length; i++){
 				tweets.push(data.statuses[i]);
 			}
@@ -54,34 +54,45 @@ T.get('search/tweets', {q: 'from:RubberNinja', count: 1, result_type: 'recent'},
 function handleInput(string){
 	var inputText = '';
 	//Get the actual input text without the twitter stuff in it
-	var parts = string.split("@RubberNinja ");
+	var parts = string.split("@NintendoAmerica ");
 	if (parts && parts[1]){
 		inputText = parts[1].toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_\'`~()]/g,"").replace(/\s{2,}/g," ");
 		var commands = inputText.split(" ");
-		
 		//Parse the Commands
 		//MOVEMENT
 		var validCommands = [];
 		if (gameState.currentState == "field"){
-			validCommands = ['north', 'south', 'east', 'west', 'up', 'down', 'left', 'right']
+			validCommands = ['north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'talk']
+			//Scan tweet for first number that's between 1 and 9, inclusive. If not found, default movement to one square.
+			var num = 1;
+			var findNum = commands.find(function(element){
+				return typeof element == "number" && element >= 1 && element <= 9;
+			});
+			if (findNum)
+				num = findNum;
 			for (var i = 0; i < commands.length; i++){
 				if (validCommands.indexOf(commands[i]) > -1){
 					switch(commands[i]){
 						case "north":
 						case "up":
-							buildImage("up");
+							buildMovementImage("up", num);
 						break;
 						case "south":
 						case "down":
-							buildImage("down");
+							buildMovementImage("down", num);
 						break;
 						case "west":
 						case "left":
-							buildImage("left");
+							buildMovementImage("left", num);
 						break;
 						case "east":
 						case "right":
-							buildImage("right");
+							buildMovementImage("right", num);
+						break;
+						case "talk":
+							var npcId = checkNextToNPC(gameState.player.direction);
+							if (npcId)
+								buildDialogImage(npcId);
 						break;
 					}
 					console.log(commands[i]);
@@ -93,7 +104,7 @@ function handleInput(string){
 	return false;
 }
 
-function buildImage(command){
+function buildMovementImage(command, stepsNum){
 	var encoder = new gifEncoder(400, 200);
 	encoder.createReadStream().pipe(fs.createWriteStream("test.gif"))
 	encoder.start();
@@ -110,11 +121,11 @@ function buildImage(command){
 				if (err) throw err;
 				console.log("done with player image");
 				
-				var distance = 128;
+				var distance = 32 * stepsNum;
 				var stepDistance = 4;
 				var i = 0;
 				
-				while (i < distance){
+				while (i <= distance){
 					var b = bg.clone();
 					var p = img.clone();
 					var m = map.clone();
@@ -122,21 +133,21 @@ function buildImage(command){
 					var y = 0;
 					switch (command){
 						case "left":
-						if (checkCollisions(gameState.player.xPosition - i, gameState.player.yPosition, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
+						if (checkCollisions(gameState.player.xPosition + i, gameState.player.yPosition, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
 							distance = i;
 						x = i;
 						break;
 						case "right":
-						if (checkCollisions(gameState.player.xPosition + i, gameState.player.yPosition, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
+						if (checkCollisions(gameState.player.xPosition - i, gameState.player.yPosition, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
 							distance = i;
 						x = -i;
 						break;
 						case "down":
-						if (checkCollisions(gameState.player.xPosition, gameState.player.yPosition - i, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
+						if (checkCollisions(gameState.player.xPosition, gameState.player.yPosition + i, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
 							distance = i;
 						y = -i;
 						case "up":
-						if (checkCollisions(gameState.player.xPosition, gameState.player.yPosition + i, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
+						if (checkCollisions(gameState.player.xPosition, gameState.player.yPosition - i, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight))
 							distance = i;
 						y = i;
 						break;
@@ -145,6 +156,13 @@ function buildImage(command){
 					if (i == distance){
 						encoder.setDelay(1500);
 						gameState.playerSprite.currentFrame = 0;
+					}
+					else if (i == 0){
+						encoder.setDelay(200);
+						gameState.playerSprite.currentFrame = 0;
+					}
+					else{
+						encoder.setDelay(75);
 					}
 					var pFrame = p.clone().crop(gameState.playerSprite.currentFrame * gameState.playerSprite.spriteWidth, gameState.playerSprite.currentAnimation * gameState.playerSprite.spriteHeight, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight);
 					b.composite(m.clone(), gameState.mapPosition.xPosition + x, gameState.mapPosition.yPosition + y);
@@ -158,25 +176,29 @@ function buildImage(command){
 				console.log("done with gif");
 				encoder.finish();
 				
+				//Update and Save the JSON
 				switch(command){
 					case "left":
+						gameState.player.direction = "left";
 						gameState.player.xPosition -= distance;
 						gameState.mapPosition.xPosition += distance;
 						break;
 					case "right":
+						gameState.player.direction = "right";
 						gameState.player.xPosition += distance;
 						gameState.mapPosition.xPosition -= distance;
 						break;
 					case "up":
+						gameState.player.direction = "up";
 						gameState.player.yPosition -= distance;
 						gameState.mapPosition.yPosition += distance;
 						break;
 					case "down":
+						gameState.player.direction = "down";
 						gameState.player.yPosition += distance;
 						gameState.mapPosition.yPosition -= distance;
 						break;
 				}
-				//Save the JSON
 				var json = JSON.stringify(gameState);
 				fs.writeFile('gameState.json', json, 'utf8', function(err){
 					if (err) throw err;
@@ -184,18 +206,112 @@ function buildImage(command){
 			});
 		});
 	});
-	//use image.composite to paste an image over a canvas
+}
+
+function buildDialogImage(npcId){
+	var encoder = new gifEncoder(400, 200);
+	encoder.createReadStream().pipe(fs.createWriteStream("test.gif"))
+	encoder.start();
+	encoder.setRepeat(0);	//0 for repeat, -1 for no-repeat
+	encoder.setDelay(75); 	//frame delay in ms
+	encoder.setQuality(10);	//image quality, 10 is default
+	var img = new jimp(400, 200, 0xFFFFFFFF, function(err, bg){
+		if (err) throw err;
+		console.log("bg done");
+		jimp.read("assets/map.png", function(err, map){
+			if (err) throw err;
+			console.log("done with map image");
+			var playerImage = jimp.read("assets/player.png", function(err, player){
+				if (err) throw err;
+				console.log("done with player image");
+				var dialogBox = new jimp(250, 100, 0xEFEFEFFF, function(err, db){
+					if (err) throw err;
+					console.log("done with dialog box");
+					var font = jimp.read("assets/player.png", function(err, font){
+						if (err) throw err;
+						//Find the NPC dialog based on the ID		
+						var npc = gameState.npcs.findIndex(function(element){
+							return element.id == npcId
+						});
+						if (npc != null){
+							var text = gameState.npcs[npc].dialog;
+							var buffer = "";
+							//Start the main loop of the gif, printing words onscreen letter by letter
+							while (buffer.length <= text.length){
+								var b = bg.clone();
+								var p = player.clone();
+								var m = map.clone();
+								var d = db.clone();
+								var f = font.clone();
+								
+								if (buffer.length == text.length){
+									encoder.setDelay(1500);
+								}
+								else if (buffer.length == 0){
+									encoder.setDelay(200);
+								}
+								else{
+									encoder.setDelay(75);
+								}
+								
+								var pFrame = p.clone().crop(gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight, gameState.playerSprite.spriteWidth, gameState.playerSprite.spriteHeight);
+								b.composite(m.clone(), gameState.mapPosition.xPosition, gameState.mapPosition.yPosition);
+								b.composite(pFrame.clone(), 192, 96);
+								if (buffer.length != 0){
+									b.composite(d.clone(), 75, 100);
+									
+								}
+								encoder.addFrame(b.bitmap.data);
+								buffer += text[buffer.length];
+							}
+						}
+						console.log("done with gif");
+						encoder.finish();
+					});
+				});
+			});
+		});
+	});
 }
 
 function checkCollisions(x, y, width, height){
 	for (var i = 0; i < gameState.objects.length; i++){
-		if (x <= gameState.objects[i].x + gameState.objects[i].width &&
-		x + width >= gameState.objects[i].x &&
-		y <= gameState.objects[i].y + gameState.objects[i].height &&
-		y + height >= gameState.objects[i].y)
+		if (x < gameState.objects[i].x + gameState.objects[i].width &&
+		x + width > gameState.objects[i].x &&
+		y < gameState.objects[i].y + gameState.objects[i].height &&
+		y + height > gameState.objects[i].y)
 		{
 			return true;
 		}
 	}
 	return false;
+}
+
+function checkNextToNPC(direction){
+	switch (direction){
+		case "left":
+			for (var i = 0; i < gameState.npcs.length; i++){
+				if (gameState.npcs[i].x + gameState.npcs[i].width == gameState.player.xPosition)
+					return gameState.npcs[i].id;
+			}
+			break;
+		case "right":
+			for (var i = 0; i < gameState.npcs.length; i++){
+				if (gameState.player.xPosition + gameState.player.width == gameState.npcs[i].x)
+					return gameState.npcs[i].id;
+			}
+			break;
+		case "up":
+			for (var i = 0; i < gameState.npcs.length; i++){
+				if (gameState.npcs[i].y + gameState.npcs[i].height == gameState.player.yPosition)
+					return gameState.npcs[i].id;
+			}
+			break;
+		case "down":
+			for (var i = 0; i < gameState.npcs.length; i++){
+				if (gameState.player.yPosition + gameState.player.height == gameState.npcs[i].y)
+					return gameState.npcs[i].id;
+			}
+	}
+	return 0;
 }
